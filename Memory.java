baa43d475b35afc9e2d1ddcb19ba4e3d3a999a7a
Nour-aldeen <nouraldeenm0@gmail.com>
2022-05-28 01:11:24 +0200

    changed memory's static array names and provided comments for explanation

--- Memory.java
+++ Memory.java
@@ -2,10 +2,12 @@ public class Memory {
 
     // it is where program instructions are stored (word addressable)
     // 16 bits / word
-    static Instruction[] instruction_Memory = new Instruction[1024];
+    // array of instructions
+    static Instruction[] instruction = new Instruction[1024];
 
     // it is where data is stored (word/byte addressable 1 word = 1 byte)
     // 8 bits / word
-    static String[] data_Memory = new String[2048];
+    // example: data[0] could be: "00001111" representing the value 15
+    static String[] data = new String[2048];
 
 }
