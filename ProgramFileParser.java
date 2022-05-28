17f603312aa29cfad7ebb10e144687375fb86909
Nour-aldeen <nouraldeenm0@gmail.com>
2022-05-29 00:30:43 +0200

    create ProgramFileParser class for parsing a text file

---
+++ ProgramFileParser.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+public class ProgramFileParser {
+
+        public String read_file_content_to_a_string(File file) {
+            /*
+            *
+            * TO BE IMPLEMENTED
+            * 
+            */ 
+            return "";
+        }
+
+
+    public String getNthLineFromString(String lines) {
+        /*
+         *
+         * TO BE IMPLEMENTED
+         *
+         * 
+         */ 
+        return "";
+    }
+}
