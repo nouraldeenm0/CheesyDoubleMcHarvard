d18c8cac0716eab471dc4b27aa389fa53e015c32
nouraldeenm0 <81125079+nouraldeenm0@users.noreply.github.com>
2022-05-30 14:07:11 +0200

    Changed Register implementation to use attribute value as an Integer instead of as a String

--- Register.java
+++ Register.java
@@ -1,11 +1,11 @@
 public class Register {
     String name;
-    String value;
+    int value;
     // A register can maximum hold a value represented by 16 bits, which is 131071
 
     public Register(String name, String value) throws RegisterValueIsOutOfBounds {
         this.name = name;
-        if (Integer.parseInt(value) > 131071 || Integer.parseInt(value) < 0) {
+        if (this.value > 131071 || this.value < 0) {
             throw new RegisterValueIsOutOfBounds("Register value is out of bounds");
         }
         else {
