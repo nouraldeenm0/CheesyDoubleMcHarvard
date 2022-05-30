885b6b4da1de3f4ca53f061d35b3890376330ddd
nouraldeenm0 <81125079+nouraldeenm0@users.noreply.github.com>
2022-05-30 14:54:06 +0200

    Update and rename RegisterValueIsOutOfBounds.java to ValueOutOfBounds.java

--- RegisterValueIsOutOfBounds.java
+++
@@ -1,8 +0,0 @@
-
-public class RegisterValueIsOutOfBounds extends Exception {
-
-    public RegisterValueIsOutOfBounds(String message) {
-        super(message);
-    }
-
-}
---
+++ ValueOutOfBounds.java
@@ -0,0 +1,8 @@
+@SuppressWarnings("serial")
+public class ValueOutOfBounds extends Exception {
+
+    public ValueOutOfBounds(String message) {
+        super(message);
+    }
+
+}
